8f4eb56d0652e63d369436e9f2f927532a84ae5e
Thiago Pereira <thiagojesuspereira@gmail.com>
2013-11-15 02:10:42 -0700

    SQLite Integration working

--- Database/DatabaseViewController.h
+++ Database/DatabaseViewController.h
@@ -7,7 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "sqlite3.h"
 
 @interface DatabaseViewController : UIViewController
+
+@property(strong,nonatomic) NSString *databasePath;
+@property(nonatomic) sqlite3 *contactDB;
+
+@property (strong, nonatomic) IBOutlet UITextField *nameField;
+@property (strong, nonatomic) IBOutlet UITextField *addressField;
+@property (strong, nonatomic) IBOutlet UITextField *phoneField;
+@property (strong, nonatomic) IBOutlet UILabel *statuslabel;
+
+- (IBAction)saveData:(id)sender;
+- (IBAction)findContact:(id)sender;
+
+-(void)checkDatabase;
 
 @end
--- Database/DatabaseViewController.m
+++ Database/DatabaseViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self checkDatabase];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +28,115 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)saveData:(id)sender {
+    
+    
+    sqlite3_stmt    *statement;
+    const char *dbpath = [_databasePath UTF8String];
+    
+    if (sqlite3_open(dbpath, &_contactDB) == SQLITE_OK)
+    {
+        
+        NSString *insertSQL = [NSString stringWithFormat:
+                               @"INSERT INTO CONTACTS (name, address, phone) VALUES (\"%@\", \"%@\", \"%@\")",
+                               self.nameField.text, self.addressField.text, self.phoneField.text];
+        
+        const char *insert_stmt = [insertSQL UTF8String];
+        sqlite3_prepare_v2(_contactDB, insert_stmt,	
+                           -1, &statement, NULL);
+        if (sqlite3_step(statement) == SQLITE_DONE)
+        {
+            self.statuslabel.text = @"Contact added";
+            self.nameField.text = @"";
+            self.addressField.text = @"";
+            self.phoneField.text = @"";
+        } else {
+            self.statuslabel.text = @"Failed to add contact";
+        }
+        sqlite3_finalize(statement);
+        sqlite3_close(_contactDB);
+    }
+
+}
+
+- (IBAction)findContact:(id)sender {
+
+    const char *dbpath = [_databasePath UTF8String];
+    sqlite3_stmt    *statement;
+    
+    if (sqlite3_open(dbpath, &_contactDB) == SQLITE_OK)
+    {
+        NSString *querySQL = [NSString stringWithFormat:
+                              @"SELECT address, phone FROM contacts WHERE name=\"%@\"",
+                              _nameField.text];
+        
+        const char *query_stmt = [querySQL UTF8String];
+        
+        if (sqlite3_prepare_v2(_contactDB,
+                               query_stmt, -1, &statement, NULL) == SQLITE_OK)
+        {
+            if (sqlite3_step(statement) == SQLITE_ROW)
+            {
+                NSString *addressField = [[NSString alloc]
+                                          initWithUTF8String:
+                                          (const char *) sqlite3_column_text(
+                                                                             statement, 0)];
+                _addressField.text = addressField;
+                NSString *phoneField = [[NSString alloc]
+                                        initWithUTF8String:(const char *)
+                                        sqlite3_column_text(statement, 1)];
+                _phoneField.text = phoneField;
+                _statuslabel.text = @"Match found";
+            } else {
+                _statuslabel.text = @"Match not found";
+                _addressField.text = @"";
+                _phoneField.text = @"";
+            }
+            sqlite3_finalize(statement);
+        }
+        sqlite3_close(_contactDB);
+    }
+
+}	
+
+-(void)checkDatabase{
+    
+    NSString *docsDir;
+    NSArray *dirPaths;
+    
+    // Get the documents directory
+    dirPaths = NSSearchPathForDirectoriesInDomains(
+                                                   NSDocumentDirectory, NSUserDomainMask, YES);
+    
+    docsDir = dirPaths[0];
+    
+    // Build the path to the database file
+    _databasePath = [[NSString alloc]
+                     initWithString: [docsDir stringByAppendingPathComponent:
+                                      @"contacts.db"]];
+    
+    NSFileManager *filemgr = [NSFileManager defaultManager];
+    
+    if ([filemgr fileExistsAtPath: _databasePath ] == NO)
+    {
+        const char *dbpath = [_databasePath UTF8String];
+        
+        if (sqlite3_open(dbpath, &_contactDB) == SQLITE_OK)
+        {
+            char *errMsg;
+            const char *sql_stmt =
+            "CREATE TABLE IF NOT EXISTS CONTACTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, ADDRESS TEXT, PHONE TEXT)";
+            
+            if (sqlite3_exec(_contactDB, sql_stmt, NULL, NULL, &errMsg) != SQLITE_OK)
+            {
+                _statuslabel.text = @"Failed to create table";
+            }
+            sqlite3_close(_contactDB);
+        } else {
+            _statuslabel.text = @"Failed to open/create database";
+        }
+    }
+    
+    //NSLog(@"Here!");
+}
 @end
